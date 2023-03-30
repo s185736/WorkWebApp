45d9d6263df4acfb3099871feb53171bdeed16b9
s185736 <65380030+s185736@users.noreply.github.com>
2023-03-30 21:23:14 +0200

    Stemple Ind/Clock In page added.

---
+++ WorkWebApp/Pages/ClockIn.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWebApp.Pages;
+
+public class ClockInModel : PageModel
+{
+    private readonly ILogger<ScheduleModel> _logger;
+
+    public ClockInModel(ILogger<ScheduleModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
