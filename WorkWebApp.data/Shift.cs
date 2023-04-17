0ba9a596aa0d33a46d080095026d419b1898388b
s185736 <65380030+s185736@users.noreply.github.com>
2023-04-17 12:57:07 +0200

    Revert "Update."
    This reverts commit 94c3dc7523dad6129366128025005afb7da905f9.

--- WorkWebApp.data/Shift.cs
+++ WorkWebApp.data/Shift.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.Runtime.InteropServices.JavaScript;
 using System.Security.Cryptography;
 
