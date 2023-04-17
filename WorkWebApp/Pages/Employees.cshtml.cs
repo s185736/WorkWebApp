7b82965e42441c15f9f52a7f38f98a2370bd8925
Flora <s215709@student.dtu.dk>
2023-04-17 12:08:47 +0200

    fixes

--- WorkWebApp/Pages/Employees.cshtml.cs
+++ WorkWebApp/Pages/Employees.cshtml.cs
@@ -10,10 +10,10 @@ public class EmployeesModel : PageModel
         _context = context;
     }
 
-    public IList<User> Users { get;set; }
+    public IList<_user> Users { get;set; }
 
     public void OnGet()
     {
-        Users = _context.users.ToList();
+        Users = _context._user.ToList();
     }
 }
