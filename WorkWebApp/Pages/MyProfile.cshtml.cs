d738f29eaa66d55346b0473b48eb1114027d93c5
s185736 <65380030+s185736@users.noreply.github.com>
2023-04-24 19:40:55 +0200

    Created a page of user, in progress.

---
+++ WorkWebApp/Pages/MyProfile.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorkWebApp.Pages;
+
+public class MyProfileModel : PageModel
+{
+    private readonly ILogger<MyProfileModel> _logger;
+
+    public MyProfileModel(ILogger<MyProfileModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+    
+}
