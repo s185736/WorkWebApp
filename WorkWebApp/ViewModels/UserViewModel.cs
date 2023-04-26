d0cb673dfb7154af6a88c1840470454dc67071fd
s185736 <65380030+s185736@users.noreply.github.com>
2023-04-27 01:15:14 +0200

    Small update.

--- WorkWebApp/ViewModels/UserViewModel.cs
+++ WorkWebApp/ViewModels/UserViewModel.cs
@@ -3,11 +3,11 @@ using WorkWebApp.data;
 
 namespace WorkWebApp.ViewModels;
 
-public class UserViewModel  
+public class UserViewModel : _user
 {
            
     public _user? viewUser { get; set; }
-    public int record_id { get; set; }
+    /*public int record_id { get; set; }
     public string firstname { get; set; }
     public string? lastname { get; set; }
     public string? mail { get; set; }
@@ -17,6 +17,6 @@ public class UserViewModel
     public string? department { get; set; }
     public string? password { get; set; }
     [NotMapped]
-    public Dictionary<DateTime, _shift> ShiftsDictionary { get; set; }
+    public Dictionary<DateTime, _shift> ShiftsDictionary { get; set; }*/
 
 }
